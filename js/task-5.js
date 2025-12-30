35715e0e783bfe0a62e09b5119e37c059801aa92
Olha_Roman <ovgrozdova79@gmail.com>
2025-12-30 12:58:38 +0100

    inline2

--- js/task-5.js
+++ js/task-5.js
@@ -12,6 +12,10 @@ function makeMessage(pizzaName, callback) {
 console.log(makeMessage("Royal Grand", makePizza)); // "Pizza Royal Grand is being prepared, please wait..."
 console.log(makeMessage("Ultracheese", deliverPizza)); // "Delivering Ultracheese pizza."
 
+function registerGuest(name, callback) {
+  console.log(`Registering ${name}!`);
+  callback(name);
+}
 registerGuest("Mango", function greet(name) {
   console.log(`Welcome ${name}!`);
 });
