afc6f336c35a9169e9b34579e326754182842186
Olha_Roman <ovgrozdova79@gmail.com>
2026-01-02 17:33:03 +0100

    tosorted2

--- js/task-5.js
+++ js/task-5.js
@@ -217,7 +217,28 @@
 //   return total + user.balance;
 // }, 0);
 
-const students = ["Jacob", "Artemis", "Solomon", "Adrian", "Kai", "Ganymede"];
+// const students = ["Jacob", "Artemis", "Solomon", "Adrian", "Kai", "Ganymede"];
 
-const inAlphabetOrder = students.toSorted();
-console.log(inAlphabetOrder); // [ "Adrian", "Artemis", "Ganymede", "Jacob", "Kai", "Solomon" ]
+// const inAlphabetOrder = students.toSorted();
+// console.log(inAlphabetOrder); // [ "Adrian", "Artemis", "Ganymede", "Jacob", "Kai", "Solomon" ]
+
+
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+];
+
+const inAscendingScoreOrder = students.toSorted(
+  (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+);
+
+const inDescendingScoreOrder = students.toSorted(
+  (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+);
+
+const inAlphabeticalOrder = students.toSorted((firstStudent, secondStudent) =>
+  firstStudent.name.localeCompare(secondStudent.name)
+);
+console.log(inAlphabeticalOrder);
